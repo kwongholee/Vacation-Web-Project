da5e3bffbbfdbef9869abd17ab5dee306a2cdcb2
kwongholee <ghlee9883@gmail.com>
2023-01-23 14:52:30 +0900

    .

--- React/shoppingmall/src/data.js
+++ React/shoppingmall/src/data.js
@@ -3,24 +3,21 @@ let products = [
       id : 0,
       title : "White and Black",
       content : "Born in France",
-      price : 120000,
-      img: 'https://codingapple1.github.io/shop/shoes1.jpg'
+      price : 120000
     },
   
     {
       id : 1,
       title : "Red Knit",
       content : "Born in Seoul",
-      price : 110000,
-      img: 'https://codingapple1.github.io/shop/shoes2.jpg'
+      price : 110000
     },
   
     {
       id : 2,
       title : "Grey Yordan",
       content : "Born in the States",
-      price : 130000,
-      img: 'https://codingapple1.github.io/shop/shoes3.jpg'
+      price : 130000
     }
   ];
 
