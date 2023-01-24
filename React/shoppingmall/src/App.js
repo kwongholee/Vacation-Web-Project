947d356dd8e0fe41533f4a5390f3298b0656bfe2
kwongholee <ghlee9883@gmail.com>
2023-01-24 20:20:51 +0900

    1.24 styled-components, component lifecycle, useEffect, ajax

--- React/shoppingmall/src/App.js
+++ React/shoppingmall/src/App.js
@@ -6,6 +6,7 @@ import { useState } from 'react';
 import Detail from './routes/Detail';
 import products from './components/data';
 import {Routes, Route, Link, useNavigate, Outlet} from 'react-router-dom';
+import axios from 'axios';
 
 function App() {
 
@@ -17,7 +18,7 @@ function App() {
 
       <Navbar bg="dark" variant="dark">
         <Container>
-          <Navbar.Brand href="#home">KwongHo Shop</Navbar.Brand>
+          <Navbar.Brand href="/">KwongHo Shop</Navbar.Brand>
           <Nav className="me-auto">
             <Nav.Link onClick={() => {navigate('/')}}>Home</Nav.Link>
             <Nav.Link onClick={() => {navigate('/about')}}>About</Nav.Link>
@@ -36,9 +37,20 @@ function App() {
                 <Products shoes={shoes} />
               </div>
             </div>
+            <button onClick={() => {
+              axios.get('https://codingapple1.github.io/shop/data2.json')
+              .then((res) => {
+                let copy = [...shoes];
+                copy.push(...res.data);
+                setShoes(copy);
+              })
+              .catch((e) => {
+                console.error(e);
+              })
+            }}>btn</button>
           </>
         } />
-        <Route path="/detail" element={<Detail />} />
+        <Route path="/detail/:id" element={<Detail  shoes={shoes}/>} />
 
         <Route path="/about" element={<About />}>
           <Route path="member" element={<div>member</div>}/>
@@ -101,3 +113,19 @@ export default App;
 // 404 page: Not Found
 // Nested Routes: 라우트 안의 또다른 라우트를 만드는 방법 (여러 유사한 페이지 필요할 때)
 // Nested Routes의 장점: route 작성이 간단해짐, 부모와 자식의 element를 동시에 보여줌(component에서 Outlet으로 어디에 보여줄지 정해줌)
+// stylded-components 라이브러리: js에서 css처럼 스타일 편집 가능(css 안 열어도 됨, 스타일이 다른 js파일로 오염되지 않음, 로딩시간 단축)
+// css 오염 방지하려면 component.module.css 사용
+// props로 컴퍼넌트 재활용가능함
+// 단 js파일이 복잡해지고, 컴퍼넌트간 import 필요하며, 협업시 css 담당 숙련도 이슈 발생할 수도?
+// component lifecycle: mount(페이지에 장착), update(페이지에서 업데이트), unmount(페이지에서 제거)
+// component lifecycle 중간중간에 코드 실행 가능
+// useEffect: mount, update시 코드 실행(랜더링 후 코드가 동작함)
+// useEffect 안에 보통 (어려운 연산, 서버에서 데이터 가져오기, 타이머 장착)을 달아놓음
+// side effect: 함수의 핵심기능과 상관없는 부가 기능
+// useEffect 실행 조건을 넣을 수 있는 곳: useEffect(() => {},[])에서 [] ([]를 비워놓으면 mount시 1회 실행, 이외에는 조건이 바뀔 때마다 실행됨)
+// useEffect 동작 전에 실행되는 코드: useEffect(() => {return() => {}})에서 return (unmount시 1회 실행됨)
+// cleanup func은 mount시 실행 안됨, unmount시 실행됨
+// 서버와 통신시 방법(GET(가져올 떄)/POST(보낼 때))과 자료(URL(서버개발자가 알고 있음))이 필요함
+// ajax 이용하면 새로고침 없이 GET/POST 요청 가능
+// axios.get('url'): axios를 이용한 get 요청
+// 
--- React/shoppingmall/src/routes/Detail.js
+++ React/shoppingmall/src/routes/Detail.js
@@ -1,19 +1,82 @@
-function Detail() {
-    return (
-      <div className="container">
-        <div className="row">
-          <div className="col-md-6">
-            <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%" />
-          </div>
-          <div className="col-md-6">
-            <h4 className="pt-5">상품명</h4>
-            <p>상품설명</p>
-            <p>120000원</p>
-            <button className="btn btn-danger">주문하기</button> 
-          </div>
-        </div>
-      </div> 
-    )
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import styled from 'styled-components';
+
+class Detail2 extends React.Component { // 옛 component에서 lifecycle 간섭 방식
+  componentDidMount() {
+    // component mount 시 코드 실행
   }
+  componentDidUpdate() {
+    // component update 시 코드 실행
+  }
+  componentWillUnmount() {
+    // component unmount 시 코드 실행
+  }
+}
+
+// let Yellowbtn = styled.button`
+//   background: ${props => props.bg};
+//   color: black;
+//   padding: 10px;
+// `
+// let Box = styled.div`
+//   background: grey;
+//   padding: 20px;
+// `
+
+// let NewBtn = styled.button(Yellowbtn); // 복사 붙여넣기
+
+function Detail(props) {
+
+  let [show, setShow] = useState(true);
+  let [count, setCount] = useState(0);
+  let {id} = useParams();
+  let [input, setInput] = useState(0);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setShow(false);
+    }, 2000);
+    return () => {
+      clearTimeout();
+    }
+  }, [count])
+
+  useEffect(() => {
+    if(isNaN(input) == true) {
+      alert('Please insert the number in input');
+    }
+  }, [input])
+
+  function findId(element) {
+    if(element.id == id) {
+      return true;
+    }
+  }
+  let array = props.shoes.find(findId);
+
+  return (
+    <div className="container">
+      {/* <Yellowbtn bg='blue'>btn</Yellowbtn> */}
+      {
+        show == true ? <div className="alert alert-warning"> 2 Seconds left~ Hurry up!</div>: null 
+      }
+      <input onChange={(e) => {setInput(e.target.value)}}></input>
+      {count}
+      <button onClick={() => {setCount(count+1)}}>btn</button>
+      <div className="row">
+        <div className="col-md-6">
+          <img src={"https://codingapple1.github.io/shop/shoes"+(Number(id)+1)+".jpg"} width="100%" />
+        </div>
+        <div className="col-md-6">
+          <h4 className="pt-5">{array.title}</h4>
+          <p>{array.content}</p>
+          <p>{array.price}</p>
+          <button className="btn btn-danger">주문하기</button> 
+        </div>
+      </div>
+    </div> 
+  )
+}
 
 export default Detail;
