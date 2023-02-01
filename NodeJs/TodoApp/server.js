f9e491a42f3fac7fc99078173c4d031306e137c8
이광호 <ghlee9883@gmail.com>
2023-02-01 13:30:29 +0900

    2.1 post, RestAPI

--- NodeJs/TodoApp/server.js
+++ NodeJs/TodoApp/server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 
+app.use(express.urlencoded({extended: true}));
+
 app.listen(8080, function(req, res) {
   console.log('listening on 8080');
 });
@@ -14,5 +16,13 @@ app.get('/pet', function(req, res) {
 });
 
 app.get('/', function(req, res) {
-  res.sendFile(__dirname + '/index.html');
+  res.sendFile(__dirname+'../html/index.html');
+});
+
+app.get('/write', function(req, res) {
+  res.sendFile(__dirname+'/write.html');
+});
+
+app.post('/add', function(req,res) {
+  res.send(req.body);
 });
