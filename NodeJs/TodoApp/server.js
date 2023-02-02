d49669b5faa12baf6bbc1ece45c3b9067f98a8b7
이광호 <ghlee9883@gmail.com>
2023-02-02 17:02:50 +0900

    2.2 db 데이터 저장, ejs

--- NodeJs/TodoApp/server.js
+++ NodeJs/TodoApp/server.js
@@ -1,11 +1,21 @@
 const express = require('express');
 const app = express();
+const MongoClient = require('mongodb').MongoClient;
+app.set('view engine', 'ejs');
 
 app.use(express.urlencoded({extended: true}));
 
-app.listen(8080, function(req, res) {
-  console.log('listening on 8080');
-});
+var db; //자료를 저장할 변수 필요
+
+MongoClient.connect('mongodb+srv://leekwongho:albert9883@nodejspractice.ax8x1qt.mongodb.net/?retryWrites=true&w=majority', function(err, client) {
+  if(err) return console.log(err);
+
+  db = client.db('todoapp'); //todoapp 이라는 database에 연결
+
+  app.listen(8080, function(req, res) {
+    console.log('listening on 8080');
+  })
+})
 
 app.get('/beauty', function(req, res) {
   res.send('Beauty Site');
@@ -16,13 +26,20 @@ app.get('/pet', function(req, res) {
 });
 
 app.get('/', function(req, res) {
-  res.sendFile(__dirname+'../html/index.html');
+  res.sendFile(__dirname+'/html/index.html');
 });
 
 app.get('/write', function(req, res) {
-  res.sendFile(__dirname+'/write.html');
+  res.sendFile(__dirname+'/html/write.html');
 });
 
 app.post('/add', function(req,res) {
-  res.send(req.body);
+  db.collection('post').insertOne({title: req.body.title, date: req.body.date}, function(err, res) {
+    console.log('Write Save');
+  })
+  res.send('Good Job');
 });
+
+app.get('/list', function(req, res) {
+  res.render('list.ejs');
+})
