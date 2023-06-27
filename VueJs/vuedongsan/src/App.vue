c684dab1e478b421141be9f73fef38c294bb3e38
kwongholee <ghlee9883@gmail.com>
2023-06-27 18:48:46 +0900

    6.27 input event, watcher, transition, sort, component lifecycle

--- VueJs/vuedongsan/src/App.vue
+++ VueJs/vuedongsan/src/App.vue
@@ -1,19 +1,26 @@
 <template>  
-  <Discount />
-
   <div class="menu">
     <a v-for="abc in menus" :key="abc">{{abc}}</a>
   </div>
 
-  <Modal @closeModal="modalOpen = false" :onerooms="onerooms" :modalNum="modalNum" :modalOpen="modalOpen" />
+  <!-- <Discount v-if="showDiscount" /> -->
+
+  <h4>지금 구매하면 {{ percent }}% 할인 ㄷㄷ</h4>
+
+  <transition name="fade">
+    <Modal @closeModal="modalOpen = false" :onerooms="onerooms" :modalNum="modalNum" :modalOpen="modalOpen" />  
+  </transition>
 
   <Products @openModal="modalOpen = true; modalNum=i" :i="i" :onerooms="onerooms" :declaration="declaration" v-for="(a, i) in onerooms" :key="i" />
-  
+
+  <button @click="sortBack()">되돌리기</button>
+  <button @click="priceSort()">가격순 정렬</button>
+  <button @click="priceSortReverse()">가격 역순 정렬</button>
 </template>
 
 <script>
 import data from './oneroom';
-import Discount from './Discount.vue';
+// import Discount from './Discount.vue';
 import Modal from './Modal.vue';
 import Products from './Products.vue';
 
@@ -21,24 +28,48 @@ export default {
   name: 'App',
   data() {
     return {
+      oneroomsOriginal: [...data],
       modalOpen: false,
       // blueColor: 'color: blue',
       menus: ['Home', 'Shop', 'About'],
       products: ['역삼동 원룸', '천호동 원룸', '마포구 원룸'],
       onerooms: data,
       declaration: [0,0,0,0,0,0],
-      modalNum: 0
+      modalNum: 0,
+      showDiscount: true,
+      percent: 30,
     }
   }, 
   methods: {
     increase(i) {
       this.declaration[i] += 1;
+    },
+    sortBack() {
+      this.onerooms = [...this.oneroomsOriginal];
+    },
+    priceSort() {
+      this.onerooms.sort(function(a,b) {return a.price - b.price})
+    },
+    priceSortReverse() {
+      this.onerooms.sort(function(a,b) {return b.price - a.price})
     }
   },
   components: {
-    Discount,
+    // Discount,
     Modal, 
     Products, 
+  },
+  mounted() {
+    while(this.percent > 0) {
+        setInterval(() => {
+        this.percent--;
+      }, 1000);
+    }
+  },
+  beforeUpdate() {
+    if(this.month == 2) {
+      alert('2개월 너무 짧지 않음? 안팜 ㅅㄱ')
+    }
   }
 }
 </script>
@@ -89,5 +120,29 @@ div {
   background: white;
   border-radius: 8px;
   padding: 20px;
+}
+
+.fade-enter-from {
+  opacity: 0;
+}
+
+.fade-enter-active {
+  transition: all 1s;
+}
+
+.fade-enter-to {
+  opacity: 1;
+}
+
+.fade-leave-from {
+  transform: translateY(0px);
+}
+
+.fade-leave-active {
+  transition: all 1s;
+}
+
+.fade-leave-to {
+  transform: translateY(-2000px);
 }
 </style>
--- VueJs/vuedongsan/src/Modal.vue
+++ VueJs/vuedongsan/src/Modal.vue
@@ -4,7 +4,8 @@
       <img :src="onerooms[modalNum].image">
       <h4>{{onerooms[modalNum].title}}</h4>
       <p>{{onerooms[modalNum].content}}</p>
-      <p>{{onerooms[modalNum].price}}원</p>
+      <input v-model.number="month">
+      <p>{{month}}개월 선택함 = {{onerooms[modalNum].price * month}}원</p>
       <button @click="$emit('closeModal')">닫기</button>
     </div>
   </div>
@@ -13,6 +14,23 @@
 <script>
 export default {
   name: 'TheModal',
+  data() {
+    return {
+      month: 1,
+    }
+  },
+  watch: {
+    month(a) {
+      if(a > 12) {
+        alert('12개월 이내로만 신청 가능');
+        this.month = 12;
+      }
+      else if(isNaN(a) == true) {
+        alert('숫자만 입력하셈');
+        this.month = 1;
+      }
+    }
+  },
   props: {
     onerooms: Array,
     modalNum: Number,
