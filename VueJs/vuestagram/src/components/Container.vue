bca0aa1e9063e55a0b8564972b3336b66eb29595
kwongholee <ghlee9883@gmail.com>
2023-06-29 17:12:24 +0900

    6.29 ajax

---
+++ VueJs/vuestagram/src/components/Container.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <Post v-for="(a,i) in data" :key="i" :a="a" />
+  </div>
+</template>
+
+<script>
+import Post from './Post.vue'
+export default {
+  name: 'TheContainer',
+  components: {
+    Post,
+  },
+  props: {
+    data: Array
+  }
+}
+</script>
+
+<style>
+
+</style>
