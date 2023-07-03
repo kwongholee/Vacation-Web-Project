ddfa070c72b8d6d4296ded8dd6d74961b63f3926
kwongholee <ghlee9883@gmail.com>
2023-07-03 16:45:22 +0900

    7.3 type alias, literal type, class, prototype, class field, interface

--- TypeScript/index.js
+++ TypeScript/index.js
@@ -1,14 +1,8 @@
-var 철수쌤 = { subject: 'math' };
-var 영희쌤 = { subject: ['science', 'english'] };
-var 민수쌤 = { subject: ['science', 'art', 'korean'] };
-function teachersubject(x) {
-    if (typeof x.subject === 'string') {
-        return x.subject;
+var object = {
+    plus: function (a, b) {
+        return a + b;
+    },
+    minus: function (a, b) {
+        return a - b;
     }
-    else {
-        return x.subject[x.subject.length - 1];
-    }
-}
-console.log(teachersubject(철수쌤));
-console.log(teachersubject(영희쌤));
-console.log(teachersubject(민수쌤));
+};
--- TypeScript/index.ts
+++ TypeScript/index.ts
@@ -1,15 +1,13 @@
-let 철수쌤 = { subject : 'math' }
-let 영희쌤 = { subject : ['science', 'english'] }
-let 민수쌤 = { subject : ['science', 'art', 'korean'] }
-
-function teachersubject(x :{subject: string|string[]}) :string {
-  if(typeof x.subject === 'string') {
-    return x.subject;
-  } else {
-    return x.subject[x.subject.length-1];
-  }
+interface Calc {
+  plus : (a :number, b :number) => number
+  minus : (a :number, b :number) => number
 }
 
-console.log(teachersubject(철수쌤));
-console.log(teachersubject(영희쌤));
-console.log(teachersubject(민수쌤));
+let object :Calc = {
+  plus(a,b) {
+    return a+b;
+  },
+  minus(a,b) {
+    return a-b;
+  }
+}
