8834a25dbd293122838373bc50cc81d5384c976e
kwongholee <ghlee9883@gmail.com>
2023-07-13 15:01:52 +0900

    7.13 flutter 기본 설정 + 기본 위젯들 배워봄

---
+++ Flutter/contact/lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Center(
+          child: Container(width: 50, height: 50, color: Colors.blue)
+        )
+    );
+  }
+}
