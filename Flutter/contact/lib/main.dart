8e87a03e0a91f335e339829cd15edd4e2f41cd62
kwongholee <ghlee9883@gmail.com>
2023-12-28 16:30:34 +0900

    12.28 박스랑 글자 디자인

--- Flutter/contact/lib/main.dart
+++ Flutter/contact/lib/main.dart
@@ -12,16 +12,28 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
         home: Scaffold(
           appBar: AppBar(title: Text('앱임'),),
-          body: Text('안녕'),
-          bottomNavigationBar: BottomAppBar(
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  Icon(Icons.phone),
-                  Icon(Icons.message),
-                  Icon(Icons.contact_page)
-                ],
-              )
+          body: Container(
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(width: 100, height: 100, color: Colors.black,),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text('캐논 DSLR 100D (단렌즈, 충전기 16기가SD 포함)', style:
+                      TextStyle(fontWeight: FontWeight.w900),),
+                    Text('성동구 행당동 끌올 10분전'),
+                    Text('210,000원'),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                      Icon(Icons.favorite),
+                      Text('4')
+                    ],)
+                  ],
+                )
+              ],
+            )
           ),
         )
     );
