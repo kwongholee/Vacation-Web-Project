efc7bf1f28443cbf1018d4867ac028636474f0cb
kwongholee <ghlee9883@gmail.com>
2024-03-25 23:52:26 +0900

    03.25 - 자식에서 부모 state 수정하는 법, TextField

--- Flutter/contact/lib/main.dart
+++ Flutter/contact/lib/main.dart
@@ -15,20 +15,27 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  var name = ["이광호", "홍길동", "장영주"];
+  var name = ["이광호", "홍길동", "김정욱"];
   var like = [0,0,0];
+  var total = 3;
+  addOne(n) {
+    setState(() {
+      total++;
+      name.add(n);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
           floatingActionButton: FloatingActionButton(onPressed: (){
             showDialog(context: context, builder: (context) {
-              return Dialog(child: Text('내용'));
+              return DialogUI(addOne: addOne);
             });
           },),
-          appBar: AppBar(),
+          appBar: AppBar(title: Text(total.toString()),),
           body: ListView.builder(
-            itemCount: 3,
+            itemCount: total,
             itemBuilder: (c, i){
               return ListTile(
                 leading: Icon(Icons.person),
@@ -38,5 +45,32 @@ class _MyAppState extends State<MyApp> {
           ),
           bottomNavigationBar: BottomAppBar(),
         );
+  }
+}
+
+class DialogUI extends StatelessWidget {
+  DialogUI({Key? key, this.addOne}) : super(key: key);
+  final addOne;
+  var inputData = "";
+
+  @override
+  Widget build(BuildContext context) {
+    return Dialog(
+      child: Container(
+        padding: EdgeInsets.all(20),
+        width: 300,
+        height: 300,
+        child: Column(
+          children: [
+            TextField(onChanged: (n) {inputData = n;},),
+            TextButton(onPressed: (){
+              addOne(inputData);
+              Navigator.pop(context);
+            }, child: Text("완료")),
+            TextButton(onPressed: (){Navigator.pop(context);}, child: Text("취소"))
+          ],
+        ),
+      ),
+    );
   }
 }
